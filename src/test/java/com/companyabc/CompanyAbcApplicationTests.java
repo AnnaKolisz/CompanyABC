3f4a1f068db75ff90c7edfa5d33075c64e987809
AnnaKolisz <annakolisz@10g.pl>
2018-05-25 17:29:32 +0200

    start project

---
+++ src/test/java/com/companyabc/CompanyAbcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.companyabc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompanyAbcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
