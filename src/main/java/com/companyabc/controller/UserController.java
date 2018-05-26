78cb75bf90f66de0a67b1517c5184d13c02d5d53
AnnaKolisz <annakolisz@10g.pl>
2018-05-26 18:43:59 +0200

    styling form for adding user

--- src/main/java/com/companyabc/controller/UserController.java
+++ src/main/java/com/companyabc/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,6 +21,11 @@ public class UserController {
 	
 	@Autowired
 	PositionRepository positionRepo;
+	
+	@GetMapping("/")
+	public String startApp() {
+		return "start";
+	}
 	
 	@RequestMapping("/addEmployee")
 	public String addNewUser(Model model) {
