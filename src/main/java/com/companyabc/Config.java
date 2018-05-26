d47946d9d207a58748a33ab1ff0fa3a8667e9b40
AnnaKolisz <annakolisz@10g.pl>
2018-05-26 18:43:35 +0200

    set up spring security

--- src/main/java/com/companyabc/Config.java
+++ src/main/java/com/companyabc/Config.java
@@ -1,21 +1,40 @@
 package com.companyabc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebSecurity
 public class Config extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		http.addFilterBefore(filter, CsrfFilter.class);
+		http.authorizeRequests().antMatchers("/CSS/**", "/js/**", "/images/**").permitAll().anyRequest().authenticated()
+				.and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("java").roles("USER");
+	}
 	
-		@Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-	        filter.setEncoding("UTF-8");
-	        filter.setForceEncoding(true);
-	        http.addFilterBefore(filter, CsrfFilter.class);
-	 }
+	@SuppressWarnings("deprecation")
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+	return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
+	
+
 }
