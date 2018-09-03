5943f33503cff1484a22573b84de33f9c5ea8d6b
AnnaKolisz <annakolisz@10g.pl>
2018-09-04 01:21:20 +0200

    save project

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,22 +1,29 @@
-//$(document).ready(function(){
-//  $("#myInput").on("keyup", function() {
-//    var value = $(this).val().toLowerCase();
-//    $("#myTable tr").filter(function() {
-//      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-//    });
-//  });
-//});
 
 window.onload = function(){
-
+      searchEmployee();
 
 };
 
+function searchEmployee() {
+      let input = document.getElementById("myInput");
+      input.addEventListener("keyup", function(event){
+        let filter = input.value.toLowerCase();
+        let table = document.getElementById("myTable");
+        let trs = table.getElementsByTagName("tr");
+        let row;
+        for(let i = 0; i < trs.length; i++) {
+             row = trs[i].getElementsByTagName("td");
+           for(let j = 0; j < 4; j++){
+                if (row[j].innerHTML.toLowerCase().indexOf(filter) > - 1) {
+                    trs[i].style.display = "";
+                    break;
+                } else {
+                    trs[i].style.display = "none";
+                }
+           }
+        }
+    });
+};
 
-function callOfficeLogin() {
-    document.getElementById('cointeinerImage').style.backgroundImage = "url('../images/officeLogin.jpg')"; 
-}
 
-function callOfficeImage(){
-	document.getElementById('officeImage').style.backgroundImage = "url('../images/office.jpg')"; 
-}
+
